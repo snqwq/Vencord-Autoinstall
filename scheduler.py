4b55d476e7aee408837a5284c5e555c6cdc0c1dc
snyeg <99054951+snqwq@users.noreply.github.com>
2026-06-11 16:28:58 -0500

    scheduler

---
+++ scheduler.py
@@ -0,0 +1,34 @@
+import subprocess
+import pathlib
+import platform
+import datetime
+import sys
+
+def check_task_exists(task_name):
+    if platform.system() != "Windows":
+        print("sorry this only works on windows rn")
+        return False
+
+    command = f'schtasks /Query /TN "{task_name}"'
+    result = subprocess.run(command, capture_output=True, text=True, shell=True)
+
+    return result.returncode == 0
+
+def register_task(task_name, script_path):
+    if platform.system() != "Windows":
+        print("sorry this only works on windows rn")
+        return False
+
+    if check_task_exists(task_name):
+        print(f"Task '{task_name}' already exists.")
+        return True
+
+    command = f'schtasks /Create /TN "{task_name}" /TR "{script_path}" /SC DAILY /ST 00:00'
+    result = subprocess.run(command, capture_output=True, text=True, shell=True)
+
+    if result.returncode == 0:
+        print(f"Task '{task_name}' registered successfully.")
+        return True
+    else:
+        print(f"Failed to register task '{task_name}'. Error: {result.stderr}")
+        return False
--- updater.py
+++ updater.py
@@ -5,34 +5,52 @@ import datetime
 import sys
 
 import downloader
+import scheduler
+
+logger = []
 
 # platform check until linux support
 if platform.system() != "Windows":
     print("sorry this only works on windows rn")
+    logger.append("unsupported platform")
     exit(1)
 
+
+# check if executable is packaged
 if getattr(sys, "frozen", False):
 
+    self_path = pathlib.Path(sys.executable)
     base_dir = pathlib.Path(sys.executable).parent
 else:
-
+    self_path = pathlib.Path(__file__)
     base_dir = pathlib.Path(__file__).parent
-
 
 
 installer_downloaded, installer_path = downloader.download_and_verify(base_dir)
 if not installer_downloaded:
     exit(1)
 
+if scheduler.check_task_exists("VencordAutoUpdate"):
+    print("Task already exists, skipping registration.")
+    logger.append("Task already exists")
+else:
+    scheduler.register_task("VencordAutoUpdate", str(self_path))
+
 result = subprocess.run(
-    [str(installer_path), "-repair", "-branch", "auto"],
-    capture_output=True,
-    text=True
+    [str(installer_path), "-repair", "-branch", "auto"], capture_output=True, text=True
 )
 
 
 with open("output.log", "a") as f:
     t = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    f.write(t + "\n")
-    f.write(result.stdout)
-    f.write(result.stderr)
+    f.write(
+        t
+        + "\n begin vencord cli output\n"
+        + result.stdout
+        + "\n"
+        + result.stderr
+        + "\n end vencord cli output\n\n"
+    )
+    f.write("log:\n")
+    for entry in logger:
+        f.write(f" - {entry}\n")
